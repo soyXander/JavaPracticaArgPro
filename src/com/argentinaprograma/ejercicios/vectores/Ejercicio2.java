50f0ff39e2bf28a7f594f8feae2d6b726d52122c
Xander.- <zn.xander@gmail.com>
2022-04-29 19:57:34 -0300

    Cambio de espacios por tabulaciones

--- src/com/argentinaprograma/ejercicios/vectores/Ejercicio2.java
+++ src/com/argentinaprograma/ejercicios/vectores/Ejercicio2.java
@@ -11,27 +11,27 @@ import java.util.Scanner;
  * @author Xander.-
  */
 public class Ejercicio2 {
-    public static void main(String[] args) {
-        int[] vector = new int[15];
-        Scanner teclado = new Scanner(System.in);
-        // Pedimos las 15 edades de los alumnos al usuario
-        for (int i = 0; i < 15; i++) {
-            System.out.print("Introduce la edad del "+ (i + 1) +"º alumno: " );
-            vector[i] = teclado.nextInt();
-        }
+	public static void main(String[] args) {
+		int[] vector = new int[15];
+		Scanner teclado = new Scanner(System.in);
+		// Pedimos las 15 edades de los alumnos al usuario
+		for (int i = 0; i < 15; i++) {
+			System.out.print("Introduce la edad del " + (i + 1) + "º alumno: ");
+			vector[i] = teclado.nextInt();
+		}
 
-        // Mostramos las edad mas grande y mas pequeña
-        int edadMayor = vector[0];
-        int edadMenor = vector[0];
-        for (int i = 0; i < 15; i++) {
-            if (vector[i] > edadMayor) {
-                edadMayor = vector[i];
-            }
-            if (vector[i] < edadMenor) {
-                edadMenor = vector[i];
-            }
-        }
-        System.out.println("La edad mayor es: " + edadMayor);
-        System.out.println("La edad menor es: " + edadMenor);
-    }
+		// Mostramos las edad mas grande y mas pequeña
+		int edadMayor = vector[0];
+		int edadMenor = vector[0];
+		for (int i = 0; i < 15; i++) {
+			if (vector[i] > edadMayor) {
+				edadMayor = vector[i];
+			}
+			if (vector[i] < edadMenor) {
+				edadMenor = vector[i];
+			}
+		}
+		System.out.println("La edad mayor es: " + edadMayor);
+		System.out.println("La edad menor es: " + edadMenor);
+	}
 }
