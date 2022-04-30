30697dc22f922f94320d165da2af785942b78875
Xander.- <zn.xander@gmail.com>
2022-04-29 21:30:15 -0300

    Practica de Vectores 5: - Programa para cargar por teclado 10 animales a un vector y luego copiar los animales a otro vector en orden invertido

---
+++ src/com/argentinaprograma/ejercicios/vectores/Ejercicio5.java
@@ -0,0 +1,47 @@
+/**
+ * Se necesita un vector que permita cargar por teclado el nombre de 10 animales. A partir de esta carga,
+ * se desea otro vector que copie los mismos nombres pero en el orden inverso, es decir,
+ * si los nombres son: perro, gato, lagartija, el nuevo vector debe contener: lagartija, gato, perro.
+ * Una vez realizado el cambio, mostrar por pantalla ambos vectores para compararlos.
+ */
+package com.argentinaprograma.ejercicios.vectores;
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+	public static void main(String[] args) {
+		String[] animales = new String[10];
+		String[] animalesInvertidos = new String[10];
+		Scanner sc = new Scanner(System.in);
+
+		// Cargamos por teclado a los 10 animales
+		for (int i = 0; i < 10; i++) {
+			System.out.print("Ingrese el nombre del " + (i + 1) + " animal: ");
+			animales[i] = sc.next();
+		}
+
+		// Agregamos los animales en orden inverso al vector
+		int j = 9;
+		for (int i = 0; i < 10; i++) {
+			animalesInvertidos[j] = animales[i];
+			j--;
+		}
+
+		// Imprimimos los vectores
+		for (int i = 0; i < 10; i++) {
+			if (i < 9) {
+				System.out.print(animales[i] + ", ");
+			} else {
+				System.out.println(animales[i]);
+			}
+		}
+		System.out.println();
+		for (int i = 0; i < 10; i++) {
+			if (i < 9) {
+				System.out.print(animalesInvertidos[i] + ", ");
+			} else {
+				System.out.println(animalesInvertidos[i]);
+			}
+		}
+	}
+}
